4c6c5845157771a408b0a8b3ddc8011ed8492bcc
paulniu <niupuyue@aliyun.com>
2018-06-13 16:25:47 +0800

    抽象类和接口

---
+++ 19_抽象类和接口/19_抽象类和接口/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  19_抽象类和接口
+//
+//  Created by 牛谱乐 on 2018/6/13.
+//  Copyright © 2018年 paulniu. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//接口描述了类的行为和功能，但是没有实现具体的操作
+//C++的接口是通过抽象类来实现的。
+//数据抽象是将实现类和相关数据分离开来
+//抽象类是没有实现细节
+
+
+//如果一个类中至少有一个虚函数的声明，则这个类就是抽象类。
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
